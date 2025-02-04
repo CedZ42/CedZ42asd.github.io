52f6d80524914a01fc76522cb7b0552997461522
Cedrick <95383675+CedZ42@users.noreply.github.com>
2025-02-04 15:53:18 +0000

    saving image-filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,7 +20,9 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
+  applyFilterNoBackground(reddify);
+  // applyFilter(decreaseBlue);
+  // applyFilter(increaseGreenByBlue);
   
 
   // do not change the below line of code
@@ -32,18 +34,55 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-
+function applyFilter (filterFunction){
+  for (var i = 0; i < image.length; i++){
+    for (var j = 0; j < image[i].length; j++){
+      var rgbString = image[i][j];
+      var rgbNumbers = rgbStringToArray(rgbString);
+      filterFunction(rgbNumbers);
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[i][j] = rgbString;
+    }
+  }
+}
 
 // TODO 7: Create the applyFilterNoBackground function
-
+function applyFilterNoBackground (filterFunction){
+  var bgColor = image[0][0];
+  for (var i = 0; i < image.length; i++){
+    for (var j = 0; j < image[i].length; j++){
+      var rgbString = image[i][j];
+      if (image[i][j] !== bgColor){
+      var rgbNumbers = rgbStringToArray(rgbString);
+      filterFunction(rgbNumbers);
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[i][j] = rgbString;
+      }
+    }
+  }
+}
 
 // TODO 5: Create the keepInBounds function
-
+function keepInBounds (num){
+ return num < 0 ? 0
+ : num > 0 && num < 255 ? num
+ : 255;
+}
 
 // TODO 3: Create reddify function
-
+function reddify (array){
+  array[RED] = 200;
+}
 
 // TODO 6: Create more filter functions
+function decreaseBlue (array){
+  var newBlue = array[BLUE] - 50;
+  array[BLUE] = keepInBounds(newBlue);
+}
 
+function increaseGreenByBlue (array){
+   var color = array[BLUE] + array[GREEN];
+   array[GREEN] = keepInBounds(color);
+}
 
 // CHALLENGE code goes below here
